080194a8f659f7607a3ce7240ee76da189d77683
Breno Augusto Ferreira <155557831+brenoafsa@users.noreply.github.com>
2025-08-17 14:27:35 -0300

    feat(post-crud-endpoints)
    creates model for Posts using django auth user model, configures serializer for Post, sets view endpoints for posts and defines api routes for them, applies new migration with post table

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,44 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, PostSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Post
 
-# Create your views here.
+# Classe para CREATE User
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+# Classe para CREATE e READ Posts
+class PostListCreate(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(creator=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(creator=self.request.user)
+        else:
+            print(serializer.errors)
+
+# Classe para DELETE Posts
+class PostDelete(generics.DestroyAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(creator=user)
+    
+class PostUpdate(generics.UpdateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(creator=user)
